2c34875d141374311af722e2a0961170512fda61
Gaurav Bhatia <GB021@atcltd.atc.org.nz>
2021-03-11 13:30:15 +1300

    Add project files.

---
+++ Data/Migrations/VisitorDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VisitorManagmentSystem.Models;
+
+namespace VisitorManagmentSystem.Data
+{
+    public class VisitorDbContext : DbContext
+    {
+        public VisitorDbContext(DbContextOptions<VisitorDbContext> options)
+            : base(options)
+        {
+        }
+    
+        public VisitorDbContext()
+        {
+
+        }
+      //Dataset for models
+      public DbSet<StaffNames> Staffnames { get; set; }
+        public DbSet<Visitors> Visitors { get; set; }
+    }
+    }
+
---
+++ Models/Visitors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VisitorManagmentSystem.Models
+{
+    public class Visitors
+    {
+      [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastNAme { get; set; }
+        public string Business { get; set; }
+        public DateTime DateIn { get; set; }
+        public DateTime Dateout { get; set; }
+        public StaffNames StaffName { get; set; }
+
+    }
+}
